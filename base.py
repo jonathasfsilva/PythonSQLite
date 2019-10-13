d31045d7f8095a74cd44a279d39cc92e23ed2673
jonathasfsilva <jonathas_ssilva@hotmail.com>
2019-10-13 15:57:00 -0300

    to func

--- base.py
+++ base.py
@@ -1,15 +1,18 @@
 import sqlite3
 
-con = sqlite3.connect('base.db')
+def createDataBase():
+    con = sqlite3.connect('base.db')
+    cursor = con.cursor()
+    sqlStr = """
+    CREATE TABLE users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL)"""
+    try:
+        cursor.execute(sqlStr)
+        con.commit()
+    except Exception as e:
+        raise
+    con.close()
 
-cursor = con.cursor()
-
-sqlStr = """
-CREATE TABLE users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-name TEXT NOT NULL,
-phone TEXT NOT NULL,
-email TEXT UNIQUE NOT NULL)"""
-
-cursor.execute(sqlStr)
-con.commit()
-con.close()
+createDataBase()
